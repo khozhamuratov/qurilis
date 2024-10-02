3de4b4ba9c430e54c7e381e60b5628b181007693
khozhamuratov <khozhamuratov.nn@gmail.com>
2024-10-02 09:22:28 +0500

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,7 @@ import React from 'react'
 const Home = () => {
 	return (
 		<div className='w-full h-screen'>
-			<div className='bg-[url(background.jpg)] w-full h-screen top-0 left-0 absolute bg-cover bg-no-repeat'></div>
-			{/* <img
-				className='w-full h-screen object-cover absolute'
-				src='background.jpg'
-				alt='LOGO'
-			/> */}
+			<div className='bg-[url(https://gmk.center/wp-content/uploads/2020/08/Facebook-Post-8.jpg)] w-full h-screen top-0 left-0 absolute bg-cover bg-no-repeat'></div>
 			<p className='z-10 text-[40px] font-serif text-slate-800 absolute right-0 mr-7 mt-7 text-right w-full'>
 				Qurilish-montaj ishlarini taqvimiy rejalashtirish
 			</p>
